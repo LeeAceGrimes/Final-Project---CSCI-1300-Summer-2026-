65945f32045f88395853f59a3813fa6bd4dd8224
LeeAceGrimes <grimes.codey.l@gmail.com>
2026-07-08 15:37:01 -0600

    Initial infrastructure, multiple commits during building

---
+++ Character.h
@@ -0,0 +1,33 @@
+// CHARACTER class - will house all stats available to Player, Pet, Enemies, and NPCs.The latter will inherit, all class information
+#ifndef CHARACTER_H
+#define CHARACTER_H
+
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,39 @@
+//Inlcude Header files 
+
+
+
+
+//include libraries
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+
+//call functions
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main() {
+
+
+
+
+
+
+
+
+
+    return 0;
+}
